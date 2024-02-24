2f7bc39dc6504846fbb8dbd46423b60b76ed1321
lipi150 <112479235+lipi150@users.noreply.github.com>
2024-02-24 23:21:48 +0530

    updated images

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,13 @@
 
-/*=============== CHANGE BACKGROUND HEADER ===============*/ 
-function scrollHeader(){
-    const header = document.getElementById('header')
-    //when the scroll is greater than 50 viewport height, add the scroll-header class to theheader tag
-    if(this.scrollY >= 50) header.classList.add('header__swipe'); else header.classList.remove('header__swipe')
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader() {
+  const header = document.getElementById('header')
+  //when the scroll is greater than 50 viewport height, add the scroll-header class to theheader tag
+  if (this.scrollY >= 50) header.classList.add('header__swipe'); else header.classList.remove('header__swipe')
 }
 window.addEventListener('scroll', scrollHeader)
 
-/*=============== SWIPER POPULAR ===============*/ 
+/*=============== SWIPER POPULAR ===============*/
 var swiper = new Swiper(".popular__container", {
   slidesPerView: 3,
   spaceBetween: 25,
@@ -21,42 +21,42 @@ var swiper = new Swiper(".popular__container", {
     prevEl: ".button-prev",
   },
 
-  breakpoints:{
-      0: {
-          slidesPerView: 1,
-      },
-      520: {
-          slidesPerView: 3,
-      },
-      950: {
-          slidesPerView: 3,
-      },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 3,
+    },
+    950: {
+      slidesPerView: 3,
+    },
   },
 });
 
 /*=============== value accordion ===============*/
 const accordionItems = document.querySelectorAll('.value__acccordion-item');
 
-accordionItems.forEach((item) =>{
+accordionItems.forEach((item) => {
   const accordionHeader = item.querySelector('.value__accordion-header')
-  accordionHeader.addEventListener('click', () =>{
+  accordionHeader.addEventListener('click', () => {
     const openItem = document.querySelector('.accordion-open')
 
     toggleItem(item)
 
-    if(openItem && openItem!== item){
+    if (openItem && openItem !== item) {
       toggleItem(openItem)
     }
   })
 })
 
-const toggleItem = (item) =>{
+const toggleItem = (item) => {
   const accordionContent = item.querySelector('.value__accordion-content');
 
-  if(item.classList.contains('accordion-open')){
+  if (item.classList.contains('accordion-open')) {
     accordionContent.removeAttribute('style')
     item.classList.remove('accordion-open')
-  }else{
+  } else {
     accordionContent.style.height = accordionContent.scrollHeight + 'px'
     item.classList.add('accordion-open')
   }
@@ -64,35 +64,35 @@ const toggleItem = (item) =>{
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 const sections = document.querySelectorAll('section[id]')
-    
-const scrollActive = () =>{
-  	const scrollY = window.pageYOffset
 
-	sections.forEach(current =>{
-		const sectionHeight = current.offsetHeight,
-			  sectionTop = current.offsetTop - 58,
-			  sectionId = current.getAttribute('id'),
-			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+const scrollActive = () => {
+  const scrollY = window.pageYOffset
 
-		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-			sectionsClass.classList.add('active-link')
-		}else{
-			sectionsClass.classList.remove('active-link')
-		}                                                    
-	})
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute('id'),
+      sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add('active-link')
+    } else {
+      sectionsClass.classList.remove('active-link')
+    }
+  })
 }
 window.addEventListener('scroll', scrollActive)
 
-/*=============== SHOW SCROLL UP ===============*/ 
-const scrollUp = () =>{
-	const scrollUp = document.getElementById('scroll-up')
-    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
-	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
-						: scrollUp.classList.remove('show-scroll')
+/*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+  const scrollUp = document.getElementById('scroll-up')
+  // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+  this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+    : scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
 
-/*=============== DARK LIGHT THEME ===============*/ 
+/*=============== DARK LIGHT THEME ===============*/
 const themeButton = document.getElementById('theme-button')
 const darkTheme = 'dark-theme'
 const iconTheme = 'bx-sun'
@@ -114,15 +114,15 @@ if (selectedTheme) {
 
 // Activate / deactivate the theme manually with the button
 themeButton.addEventListener('click', () => {
-    // Add or remove the dark / icon theme
-    document.body.classList.toggle(darkTheme)
-    themeButton.classList.toggle(iconTheme)
-    // We save the theme and the current icon that the user chose
-    localStorage.setItem('selected-theme', getCurrentTheme())
-    localStorage.setItem('selected-icon', getCurrentIcon())
+  // Add or remove the dark / icon theme
+  document.body.classList.toggle(darkTheme)
+  themeButton.classList.toggle(iconTheme)
+  // We save the theme and the current icon that the user chose
+  localStorage.setItem('selected-theme', getCurrentTheme())
+  localStorage.setItem('selected-icon', getCurrentIcon())
 })
 
-/*=============== SCROLL REVEAL ANMATION ===============*/ 
+/*=============== SCROLL REVEAL ANMATION ===============*/
 const sr = ScrollReveal({
   origin: 'top',
   distance: '60px',
@@ -135,10 +135,10 @@ sr.reveal('.home__title, .popular__container, .section__subtitle, .subscribe__co
 sr.reveal('.home__description, .section__title, .footer__info', { delay: 500 })
 sr.reveal('.search__box', { delay: 600 })
 sr.reveal('.value__box', { delay: 700 })
-sr.reveal('.home__imgages', { delay: 800, origin:'bottom'})
-sr.reveal('.logos__img', {interval: 100})
-sr.reveal('.value__images, .contact__content', {origin: 'left'})
-sr.reveal('.value__content, .contact__images', {origin: 'right'})
+sr.reveal('.home__imgages', { delay: 800, origin: 'bottom' })
+sr.reveal('.logos__img', { interval: 100 })
+sr.reveal('.value__images, .contact__content', { origin: 'left' })
+sr.reveal('.value__content, .contact__images', { origin: 'right' })
 
 
 const subscribe_btn = document.querySelector('.subscribe__button');
@@ -154,3 +154,31 @@ close_btn.addEventListener('click', () => {
   popup.classList.remove('popup__active');
   document.body.style.overflow = 'auto';
 })
+
+
+//news cards
+
+// const newsImg = [
+
+//   '../img/home.jpg',
+
+// ]
+
+// const newsContainer = document.getElementsByClassName('swiper-wrapper')[0];
+
+
+
+// newsImg.forEach(news => {
+//   const newsHTML = `
+//     <article class="popular__card swiper-slide">
+//       <img src=${news} alt="apartment picture">
+//       <div class="card__info">
+//         <h3 class="card__title">Garden City Assat</h3>
+//         <p class="card__description">
+//           Street The garden City Of Miraflores, Lima - Peru Av.Sol #9876
+//         </p>
+//       </div>
+//     </article>
+//   `;
+//   newsContainer.innerHTML += newsHTML;
+// });
---
+++ assets/js/newsImg.js
@@ -0,0 +1,35 @@
+const newsImg = [
+    'file:///C:/Users/Lenovo/reactfiles/prema-foundation/assets/img/mainImage.jpeg',
+    'C:/Users/Lenovo/reactfiles/prema-foundation/assets/img/MPF news image/1707906500081.jpg',
+    'C:/Users/Lenovo/reactfiles/prema-foundation/assets/img/MPF news image/1707906500118.jpg',
+    'C:/Users/Lenovo/reactfiles/prema-foundation/assets/img/MPF news image/1707906500156.jpg',
+    'C:/Users/Lenovo/reactfiles/prema-foundation/assets/img/MPF news image/1707906500194.jpg',
+    'C:/Users/Lenovo/reactfiles/prema-foundation/assets/img/MPF news image/1707906500235.jpg',
+    'C:/Users/Lenovo/reactfiles/prema-foundation/assets/img/MPF news image/1707906500277.jpg',
+    'C:/Users/Lenovo/reactfiles/prema-foundation/assets/img/MPF news image/1707906500316.jpg',
+    'C:/Users/Lenovo/reactfiles/prema-foundation/assets/img/MPF news image/1707906500347.jpg',
+    'C:/Users/Lenovo/reactfiles/prema-foundation/assets/img/MPF news image/1707906500376.jpg',
+    'C:/Users/Lenovo/reactfiles/prema-foundation/assets/img/MPF news image/1707906500404.jpg',
+    'C:/Users/Lenovo/reactfiles/prema-foundation/assets/img/MPF news image/1707906500430.jpg',
+    'C:/Users/Lenovo/reactfiles/prema-foundation/assets/img/MPF news image/1707906500447.jpg',
+    'C:/Users/Lenovo/reactfiles/prema-foundation/assets/img/MPF news image/1707906500474.jpg',
+    'C:/Users/Lenovo/reactfiles/prema-foundation/assets/img/MPF news image/1707906500501.jpg',
+    'C:/Users/Lenovo/reactfiles/prema-foundation/assets/img/MPF news image/1707906500524.jpg',
+    'C:/Users/Lenovo/reactfiles/prema-foundation/assets/img/MPF news image/1707906500547.jpg',
+    'C:/Users/Lenovo/reactfiles/prema-foundation/assets/img/MPF news image/1707906500568.jpg',
+    'C:/Users/Lenovo/reactfiles/prema-foundation/assets/img/MPF news image/1707906500589.jpg',
+    'C:/Users/Lenovo/reactfiles/prema-foundation/assets/img/MPF news image/1707906500612.jpg',
+    'C:/Users/Lenovo/reactfiles/prema-foundation/assets/img/MPF news image/1707906500637.jpg',
+    'C:/Users/Lenovo/reactfiles/prema-foundation/assets/img/MPF news image/1707906500661.jpg',
+    'C:/Users/Lenovo/reactfiles/prema-foundation/assets/img/MPF news image/1707906500691.jpg',
+    'C:/Users/Lenovo/reactfiles/prema-foundation/assets/img/MPF news image/1707906500714.jpg',
+    'C:/Users/Lenovo/reactfiles/prema-foundation/assets/img/MPF news image/1707906500735.jpg'
+
+
+
+
+
+
+]
+
+export default newsImg
